0ff4a4bfcdaa7501630b06ad3168b5c4b41c0071
gt-shetty <yashwanthshetty23@gmail.com>
2023-10-27 13:17:42 +0530

    db connection done

--- Resources/Bodies.py
+++ Resources/Bodies.py
@@ -1,3 +1,7 @@
+import json
+import requests
+import pymongo
+
 from robot.api.deco import library, keyword
 from robot.libraries.BuiltIn import BuiltIn
 
@@ -7,18 +11,40 @@ class Bodies:
 
     @keyword
     def get_device_info_body(self):
+        payload = {
+            "camera_device_id": "56AI010100000017",
+            "event_name": "MESSAGE/64b9297fc35b1d648623bd3a",
+            "data": "{\"event\": \"GET_DEVICE_INFO\", \"udid\": \"56AI010100000017\",\"timestamp\": \"1682077731\", \"id\": \"63ce55886b9ce300f9831f6b\", \"request_id\":\"87e5d0e7-6e10-4f27-877a-1b9d1a19dd2d\" }"
+        }
+
+    @keyword
+    def get_wifi_settings_body(self):
+        payload = {
+            "camera_device_id": "56AI010100000017",
+            "event_name": "MESSAGE/64b9297fc35b1d648623bd3a",
+            "data": "{\"event\": \"WIFI_NETWORK_CONFIG\", \"udid\": \"56AI010100000018\",\"timestamp\": \"1682077731\", \"id\": \"63ce55886b9ce300f9831f6b\", \"request_id\":\"87e5d0e7-6e10-4f27-877a-1b9d1a19dd2d\", \"state\": \"ENABLE\",\"ssid\": \"56Secure J\",\"password\":  \"56Secure@123$\",\"security\": \"WPA2\"}"
+        }
+        return payload
+
+    @keyword
+    def get_onecam_headers(self):
         temp = {
-                    "camera_device_id": "56AI010100000017",
-                    "event_name": "MESSAGE/64b9297fc35b1d648623bd3a",
-                    "data": "{\"event\": \"GET_DEVICE_INFO\", \"udid\": \"56AI010100000017\",\"timestamp\": \"1682077731\", \"id\": \"63ce55886b9ce300f9831f6b\", \"request_id\":\"87e5d0e7-6e10-4f27-877a-1b9d1a19dd2d\" }"
+                  'X-Device-Id': 'postman',
+                  'X-Platform': 'android',
+                  'X-Version': '10.0.0',
+                  'Content-Type': 'application/json'
                 }
 
 
         return temp
 
     @keyword
-    def get_onecam_headers(self):
-
-
-
-        return temp
+    def test_mongodb(self):
+        mongo_client = pymongo.MongoClient("mongodb+srv://write:aZIMmWzMOa8VMDNl@staging.9f7yz.mongodb.net/?authSource=admin")
+        db = mongo_client.vision_qa
+        collection = db.camera_account
+        document = collection.find_one({"camera_custom_id":"56AI010100000015", "status":"active"})
+        if document:
+            print(document)
+        else:
+            print("Document not found")
--- Tests/testDemo1.robot
+++ Tests/testDemo1.robot
@@ -1,18 +1,38 @@
 *** Settings ***
 Library     RequestsLibrary
 Library      ../Resources/Bodies.py
-Library ../Resources/Bodies.py
+Library    DatabaseLibrary
+
+
 
 *** Variables ***
+${count}   0
 
 
 *** Test Cases ***
 
 Get device info
-    reverse a string
-    ${get_device_info_body}         get device info body
-    ${onecam_headers}         get_onecam_headers
-    POST        https://api.qa.56secure.com/rpc/v1/raven/send_message    json= ${get_device_info_body}      expected_status=200   headers=${onecam_headers}
-    sleep   5
 
-# comment by smw
+    ${get_device_info_body}         get device info body
+    ${onecam_headers}         get onecam headers
+    POST        https://api.qa.56secure.com/rpc/v1/raven/send_message    json=${get_device_info_body}    expected_status=200   headers=${onecam_headers}
+    sleep   5
+    ${count}  Evaluate     ${count} + 1
+    Log To Console     call...${count}
+
+
+
+Change_wifi_settings
+
+#    Connect To Database Using Custom Connection String    dbapiModuleName=pymongo    db_connect_string=mongodb+srv://write:aZIMmWzMOa8VMDNl@staging.9f7yz.mongodb.net/?authSource=admin
+#
+#    ${query} =    Evaluate    {camera_custom_id:"56AI010100000015", status:"active"}    json
+#    ${result} =    Run Keyword    Find All Documents    vision_qa.camera_account     ${query}
+#    Log    Value from MongoDB: ${result[0]['tag']}
+
+    test mongodb
+    ${get_wifi_settings_body}         get wifi settings body
+    ${onecam_headers}         get onecam headers
+    POST        https://api.qa.56secure.com/rpc/v1/raven/send_message    json=${get_wifi_settings_body}    expected_status=200   headers=${onecam_headers}
+    sleep   5
+    Log To Console     call...${count}
